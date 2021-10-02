67a35e511dd1444208c0912981f307f3da541f52
sheila <sheila18.sa@gmail.com>
2021-10-02 13:52:44 +0200

    request to api

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,41 @@ import callToApi from './services/api';
 import ls from './services/local-storage'
 import PropTypes from 'prop-types';
 import '../styles/App.scss';
+import title from '../images/Rick_and_Morty_-_logo_(English).png'
 
 function App() {
+  const [ characters, setCharacters]=useState ([]);
+
+  useEffect(()=>{
+    callToApi().then((response)=>{
+      setCharacters (response);
+    });
+  },[]); 
+
+  const renderCharacters=()=>{
+    return characters.map((characters, index) =>{
+      return (
+        <li key={index}>
+          <img src={characters.img} alt ={characters.name}/>
+          <h2>{characters.name}</h2>
+          <h3>{characters.species}</h3>
+        </li>
+      );
+    });
+  };
   return (
     <div>
-      <h1>Hola mundo</h1>
-      
+     <header className="header">
+      <img src={title} alt="rick and morty" className="header_title" />
+      <form>
+        <label>Busca tu personaje favorito</label>
+        <input type="text" />
+
+      </form>
+     </header>
+      <main>
+        <ul>{renderCharacters()}</ul>
+      </main>
     </div>
   );
 }
---
+++ src/components/services/api.js
@@ -0,0 +1,18 @@
+const result= 20;
+const callToApi=()=>{
+    return fetch ('https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json')
+    .then(response=>response.json())
+    .then (data=>{
+        const result =data.results.map((item)=>{
+            return{
+                name: item.name,
+                img : item.image,
+                species: item.especies
+            };
+
+
+        });
+        return result;
+    });
+};
+export default callToApi;
