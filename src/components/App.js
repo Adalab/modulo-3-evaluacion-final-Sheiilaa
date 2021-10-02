464002b36bc8fc3f660a1803ebc369ca3e415884
sheila <sheila18.sa@gmail.com>
2021-10-02 11:21:35 +0200

    create folders

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import callToApi from './services/api';
+import ls from './services/local-storage'
+import PropTypes from 'prop-types';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      
+    </div>
+  );
+}
+
+export default App;
